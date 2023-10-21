6a41bebc5d8d285b6df234e833ed54069bedbc7b
BabyBlue52 <chris.hennemann256@gmail.com>
2023-10-20 19:25:20 -0500

    Refactoring stories

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <Docs />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import Docs from '@/components/Introduction.vue'
 </script>
 <style lang="scss">
   @import "./assets/main.scss";
