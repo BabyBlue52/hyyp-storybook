cbc5db235f3a9143e2da14b78efd6b2d0db79f3b
BabyBlue52 <chris.hennemann256@gmail.com>
2025-01-13 13:58:00 -0600

    Ready storybook-static

--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -1,48 +1,28 @@
-import { fn } from '@storybook/test';
+import Header from '../components/Header';
+import { action } from '@storybook/addon-actions';
 
-import MyHeader from './Header.vue';
 
 export default {
-  title: 'Example/Header',
-  component: MyHeader,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
-  render: (args) => ({
-    // Components used in your story `template` are defined in the `components` object
-    components: {
-      MyHeader,
-    },
-    // The story's `args` need to be mapped into the template through the `setup()` method
+    component: Header, 
+    title: 'System / Header',
+    decorators: [() => ({ template: '<div style="max-width:95vw;margin: 0 auto;"><story/></div>' })], // Add decorators to component, view full-width items    
+};
+
+const Template = args => ({
+    components: { Header },
     setup() {
-      // Story args can be spread into the returned object
-      return {
-        ...args,
-      };
+        return {args};
     },
-    // Then, the spread values can be accessed directly in the template
-    template: '<my-header :user="user" />',
-  }),
-  parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
-    layout: 'fullscreen',
-  },
-  args: {
-    onLogin: fn(),
-    onLogout: fn(),
-    onCreateAccount: fn(),
-  },
-};
+    template: '<Header v-bind="args"/>',
+})
+export const Basic = Template.bind({
+    userAuth: false,
+    isPremium: false,
+    userInitials: 'DK',
+});
 
-export const LoggedIn = {
-  args: {
-    user: {
-      name: 'Jane Doe',
-    },
-  },
-};
-
-export const LoggedOut = {
-  args: {
-    user: null,
-  },
-};
+export const Premium = Template.bind({
+    userAuth: true,
+    isPremium: true,
+    userInitials: 'DK',
+});
--- storybook-static/assets/hyyp-3ee48e0f.js
+++ storybook-static/assets/hyyp-3ee48e0f.js
@@ -1,2 +1,1 @@
 const e=""+new URL("hyyp-5b11714e.svg",import.meta.url).href;export{e as _};
-//# sourceMappingURL=hyyp-3ee48e0f.js.map
