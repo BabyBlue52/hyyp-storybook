fa47914c7d3f19b96cef31a86d4c21f708a19cbb
BabyBlue52 <chris.hennemann256@gmail.com>
2026-07-15 18:43:43 -0500

    Add additional component styles, sync with clientApp

--- src/components/Buttons/SecondaryButton.vue
+++ src/components/Buttons/SecondaryButton.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
     <button
         type="button"
         class="secondary-btn"
@@ -7,6 +8,10 @@
         @click="onClick"
     >
         <span>{{ text }}</span>
+=======
+    <button class="secondary-btn" :class="[disabled ? 'disabled' : '']">
+        <span>{{text}}</span>
+>>>>>>> 9870d21 (Add additional component styles, sync with clientApp)
         <v-icon v-if="icon" :class="['mdi', iconName]" />
     </button>
 </template>
@@ -20,17 +25,23 @@ const props = defineProps({
     disabled: Boolean,
 })
 
+<<<<<<< HEAD
 const emit = defineEmits(['click'])
 
+=======
+>>>>>>> 9870d21 (Add additional component styles, sync with clientApp)
 const iconName = computed(() => {
     if (!props.icon) return ''
     return props.icon.startsWith('mdi-') ? props.icon : `mdi-${props.icon}`
 })
+<<<<<<< HEAD
 
 function onClick(event) {
     if (props.disabled) return
     emit('click', event)
 }
+=======
+>>>>>>> 9870d21 (Add additional component styles, sync with clientApp)
 </script>
 <style lang="scss" scoped>
 @use "sass:color";
--- src/stories/Buttons/SecondaryButton.stories.js
+++ src/stories/Buttons/SecondaryButton.stories.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import SecondaryButton from '../../components/Buttons/SecondaryButton.vue';
+=======
+import SecondaryButton from '../../components/Buttons/SecondaryButton';
+>>>>>>> 9870d21 (Add additional component styles, sync with clientApp)
 
 export default {
     component: SecondaryButton,
@@ -15,25 +19,42 @@ const Template = (args) => ({
     setup() {
         return { args };
     },
+<<<<<<< HEAD
     template: '<SecondaryButton v-bind="args" @click="args.onClick" />',
+=======
+    template: '<SecondaryButton v-bind="args"/>',
+>>>>>>> 9870d21 (Add additional component styles, sync with clientApp)
 });
 
 export const Default = Template.bind({});
 Default.args = {
     text: 'Secondary Button',
+<<<<<<< HEAD
     onClick: () => {},
+=======
+    disabled: false,
+>>>>>>> 9870d21 (Add additional component styles, sync with clientApp)
 };
 
 export const WithIcon = Template.bind({});
 WithIcon.args = {
+<<<<<<< HEAD
     text: 'Save calendar',
     icon: 'check',
     onClick: () => {},
+=======
+    text: 'Edit',
+    icon: 'pencil',
+    disabled: false,
+>>>>>>> 9870d21 (Add additional component styles, sync with clientApp)
 };
 
 export const Disabled = Template.bind({});
 Disabled.args = {
     text: 'Secondary Button',
     disabled: true,
+<<<<<<< HEAD
     onClick: () => {},
+=======
+>>>>>>> 9870d21 (Add additional component styles, sync with clientApp)
 };
